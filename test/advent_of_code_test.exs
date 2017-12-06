59dd87dd8d24fc54640096fa7288c8adabfadb2f
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2017-12-07 00:18:03 +0100

    Remove duplicated tests

--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -6,19 +6,4 @@ defmodule AdventOfCodeTest do
     assert AdventOfCode.hello() == :world
   end
 
-  test "1122 produces a sum of 3 (1 + 2)" do
-    assert AdventOfCode.Day1.solve("1122") == 3
-  end
-
-  test "each digit matches the next" do
-    assert AdventOfCode.Day1.solve("1111") == 4
-  end
-
-  test "no digit matches the next" do
-    assert AdventOfCode.Day1.solve("1234") == 0
-  end
-
-  test "last digit matches the first" do
-    assert AdventOfCode.Day1.solve("91212129") == 9
-  end
 end
