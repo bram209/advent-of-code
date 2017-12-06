93d711d59d442f8cd183760a078fda3421f38f61
Bram Hoendervangers <bhoendervangers@vaa.com>
2017-12-06 18:18:39 +0100

    Day 1 tests

--- test/day1/day1_test.exs
+++ test/day1/day1_test.exs
@@ -1,11 +1,6 @@
-defmodule AdventOfCodeTest do
+defmodule AdventOfCode.Day1Test do
   use ExUnit.Case
-  doctest AdventOfCode
-
-  test "greets the world" do
-    assert AdventOfCode.hello() == :world
-  end
-
+  
   test "1122 produces a sum of 3 (1 + 2)" do
     assert AdventOfCode.Day1.solve("1122") == 3
   end
