6a7d3c29230dd36816b1422df70eb1620c37218c
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2017-12-07 22:02:17 +0100

    Completed day 2 - part 2

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -1,9 +1,30 @@
 defmodule AdventOfCode.Day2 do
-  def sum(input) do
+  def solve(input, :part1), do: solve(input, &checksum1/1)
+  def solve(input, :part2), do: solve(input, &checksum2/1)
+
+  def solve(input, compute_checksum) do
     input
-    |> Stream.map(&{Enum.min(&1), Enum.max(&1)})
-    |> Stream.map(fn {min, max} -> abs(min - max) end) # TODO improve performance by calculating min and max in one go -> Reduce?
-    |> Enum.sum    
+    |> Stream.map(&compute_checksum.(&1))
+    |> Enum.sum
+  end
+
+  def checksum1(row) do
+    abs(Enum.min(row) - Enum.max(row))
+  end
+
+  def checksum2(row) do
+    row
+    |> permutations
+    |> Enum.map(fn {a,b} -> {max(a, b), min(a,b)} end)
+    |> Enum.filter(fn {a, b} -> rem(a, b) == 0 end)
+    |> Enum.map(fn {a, b} -> Integer.floor_div(a, b) end)
+    |> Enum.sum
+  end
+
+  def permutations(input) do
+    for {a, i} <- Enum.with_index(input),
+        b <- Enum.drop(input, i + 1), # Make sure to have unique combinations. For example, we don't want {1,2} AND {2,1}, we neither want {1,1}, {2,2} etc
+        do: {a, b}
   end
 
   def parse(file_name) do
--- test/day2/day2_test.exs
+++ test/day2/day2_test.exs
@@ -3,13 +3,23 @@ defmodule AdventOfCode.Day2Test do
 
   alias AdventOfCode.Day2
 
-  @example [
+  @example1 [
     [5, 1, 9, 5],
     [7, 5, 3],
     [2, 4, 6, 8]
   ]
 
-  test "solving the example should produce a sum of 18 (8 + 4 + 6)" do
-    assert Day2.sum(@example) == 18
+  @example2 [
+    [5, 9, 2, 8],
+    [9, 4, 7, 3],
+    [3, 8, 6, 5]
+  ]
+
+  test "solving part 1 with the example should produce a sum of 18 (8 + 4 + 6)" do
+    assert Day2.solve(@example1, :part1) == 18
+  end
+
+  test "solving part 2 with the example should produce a sum of 9 (4 + 3 + 2)" do
+    assert Day2.solve(@example2, :part2) == 9 
   end
 end
