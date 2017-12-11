f250361e01da545b8ba0e42d785fc8332aa8e2b6
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2017-12-11 20:04:44 +0100

    Completed day 3 - part 2

--- lib/day3/day3.ex
+++ lib/day3/day3.ex
@@ -3,33 +3,55 @@ defmodule AdventOfCode.Day3 do
     defstruct [:value, :x, :y]
   end
 
-  # 17 16 15 14 13
-  # 18 5  4  3  12
-  # 19 6  1  2  11
-  # 20 7  8  9  10
-  # 21 22 23 24 25
-  # right(1) up(size-2), left(size-1), down(size-1), right(size-1)
+  def solve(input, _) when input == 1, do: 0
+  def solve(input, part) when is_number(input) and input > 1, do: _solve(input, part)
 
-  def solve(input) when input == 1, do: 0
-
-  def solve(input) when is_number(input) and input > 1 do
-    build_path()
-    |> Stream.drop_while(&(&1.value != input))
-    |> Enum.take(1)
-    |> Enum.map(&(abs(&1.x) + abs(&1.y)))
-    |> hd()
+  defp _solve(input, :part1) do
+    build_path(:part1)
+    |> Enum.find(&(&1.value == input))
+    |> to_distance
   end
 
+  defp _solve(input, :part2) do
+    build_path(:part2)
+    |> Stream.map(&(&1.value))
+    |> Enum.find(&(&1 > input))
+  end
+
+  def to_distance(value), do: abs(value.x) + abs(value.y)
+
   # Maps the infinite spiral stream, that emits relative coordinates, to squares
-  def build_path do
+  def build_path(:part1) do
     build_spiral()
-    |> Stream.scan(%Square{value: 1, x: 0, y: 0}, fn {delta_x, delta_y},%Square{value: value, x: x, y: y} ->
+    |> Stream.scan(%Square{value: 1, x: 0, y: 0}, fn {delta_x, delta_y}, %Square{value: value, x: x, y: y} ->
          %Square{value: value + 1, x: x + delta_x, y: y + delta_y}
        end)
   end
 
+  def build_path(:part2) do
+    build_spiral()
+    |> Stream.transform({%{{0, 0} => 1}, %Square{value: 1, x: 0, y: 0}}, fn {delta_x, delta_y}, {mem, %Square{ x: x, y: y }} ->
+         cords = {x + delta_x, y + delta_y}
+         value = sum_neighbours(mem, cords)
+         transformed = %Square{value: value, x: x + delta_x, y: y + delta_y}
+
+         {
+           [transformed],
+           {Map.put(mem, cords, value), transformed}
+         }
+       end)
+  end
+
+  def sum_neighbours(mem, {x_loc, y_loc}) do
+    for(i <- (x_loc - 1)..(x_loc + 1), do: for(j <- (y_loc - 1)..(y_loc + 1), do: {i, j}))
+    |> List.flatten()
+    |> Stream.map(&Map.get(mem, &1))
+    |> Stream.filter(&(&1 != nil))
+    |> Enum.sum()
+  end
+
   # Generate an infinite spiral stream
-  def build_spiral do
+  def build_spiral() do
     Stream.resource(
       fn -> 3 end,
       fn box_size -> {spiral_steps(box_size), box_size + 2} end,
--- test/day3/day3_test.exs
+++ test/day3/day3_test.exs
@@ -4,12 +4,13 @@ defmodule AdventOfCode.Day3Test do
   alias AdventOfCode.Day3
 
   test "data from square 1 is carried 0 steps" do
-    assert Day3.solve(1) == 0
+    assert Day3.solve(1, :part1) == 0
+    assert Day3.solve(1, :part2) == 0
   end
 
   test "data is carried the correct amount of steps" do
-    assert Day3.solve(12) == 3
-    assert Day3.solve(23) == 2
-    assert Day3.solve(1024) == 31
+    assert Day3.solve(12, :part1) == 3
+    assert Day3.solve(23, :part1) == 2
+    assert Day3.solve(1024, :part1) == 31
   end
 end
