1cf342c5b0f7f4eb91416123f4c46a7046886270
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2017-12-12 21:10:57 +0100

    Completed day 4 - part 2

--- lib/day4/day4.ex
+++ lib/day4/day4.ex
@@ -1,39 +1,35 @@
 defmodule AdventOfCode.Day4 do
   def solve(phrase, part) when is_binary(phrase), do: solve([phrase], part)
+  
+  def solve(phrases, :part1), do: _solve(phrases, &is_not_uniq/1)
+  def solve(phrases, :part2), do: _solve(phrases, &has_anagrams/1)
 
-  def solve(phrase, :part1) do
+  defp _solve(phrases, reject_fun) do
     phrases
     |> Stream.map(&String.split/1)
-    |> Stream.reject(&is_not_uniq/1)
-    |> Enum.count()
-  end
-
-  def solve(phrases, :part2) do
-    phrases
-    |> Stream.map(&String.split/1)
-    |> Stream.reject(&has_anagrams/1)
+    |> Stream.reject(reject_fun)
     |> Enum.count()
   end
 
   def has_anagrams(words) do
     words
-    |> Stream.map(&String.to_charlist/1)
-    |> Stream.map(&MapSet.new/1)
-    |> permutations
-    |> Stream.filter(fn {a, b} -> MapSet.equal?(a, b) end)
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.map(&Enum.sort/1)
+    |> permutations()
+    |> Stream.filter(fn {a, b} -> a == b end)
     |> Enum.any?()
-  end
-
-  def permutations(input) do
-    for {a, i} <- Stream.with_index(input),
-        b <- Stream.drop(input, i + 1),
-        do: {a, b}
   end
 
   def is_not_uniq(enumarable) do
     enumarable
     |> Stream.transform(MapSet.new(), &{[MapSet.member?(&2, &1)], MapSet.put(&2, &1)})
     |> Enum.any?()
+  end
+
+  def permutations(input) do
+    for {a, i} <- Enum.with_index(input),
+        b <- Enum.drop(input, i + 1),
+        do: {a, b}
   end
 
   def read(file_name) do
--- test/day4/day4_test.exs
+++ test/day4/day4_test.exs
@@ -3,16 +3,29 @@ defmodule AdventOfCode.Day4Test do
 
   alias AdventOfCode.Day4
 
-  test "pass phrase is valid" do
-    assert Day4.solve("aa bb cc dd ee") == 1
-    assert Day4.solve("aa bb cc dd aaa") == 1
+  # Part 1
+  test "pass phrase is unique" do
+    assert Day4.solve("aa bb cc dd ee", :part1) == 1
+    assert Day4.solve("aa bb cc dd aaa", :part1) == 1
   end
 
-  test "pass phrase with duplicate values is not valid" do
-    assert Day4.solve("aa bb cc dd aa") == 0
+  test "pass phrase with duplicate values is not unique" do
+    assert Day4.solve("aa bb cc dd aa", :part1) == 0
   end
 
-  test "2 out of 3 phrases are correct" do
-    assert Day4.solve(["aa bb cc dd aaa", "aa bb cc dd ee", "aa bb cc dd aa"]) == 2
+  test "2 out of 3 phrases are unique" do
+    assert Day4.solve(["aa bb cc dd aaa", "aa bb cc dd ee", "aa bb cc dd aa"], :part1) == 2
   end
+
+  # Part 2
+  test "pass phrase has no anagrams" do
+    assert Day4.solve("abcde fghij", :part2) == 1
+    assert Day4.solve("a ab abc abd abf abj", :part2) == 1
+    assert Day4.solve("iiii oiii ooii oooi oooo", :part2) == 1
+  end
+
+  test "pass phrase has anagrams" do
+    assert Day4.solve(["oiii ioii iioi iiio", "abcde xyz ecdab"], :part2) == 0
+  end
+
 end
