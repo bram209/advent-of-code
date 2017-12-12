fbe7852634ed4981719ed9dc797b72e9726e5867
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2017-12-12 20:00:39 +0100

    Completed day 4 - part 1

--- lib/day4/day4.ex
+++ lib/day4/day4.ex
@@ -1,3 +1,16 @@
 defmodule AdventOfCode.Day4 do
+  def solve(phrase) when is_binary(phrase), do: solve([phrase])
 
+  def solve(phrases) when is_list(phrases) do
+    phrases
+    |> Stream.map(&String.split/1)
+    |> Stream.reject(&is_not_uniq/1)
+    |> Enum.count
+  end
+
+  def is_not_uniq(enumarable) do
+    enumarable
+    |> Stream.transform(MapSet.new(), &({[MapSet.member?(&2, &1)], MapSet.put(&2, &1)}))
+    |> Enum.any?()
+  end
 end
--- test/day4/day4_test.exs
+++ test/day4/day4_test.exs
@@ -4,11 +4,15 @@ defmodule AdventOfCode.Day4Test do
   alias AdventOfCode.Day4
 
   test "pass phrase is valid" do
-    assert Day4.solve("aa bb cc dd ee") == true
-    assert Day4.solve("aa bb cc dd aaa") == true
+    assert Day4.solve("aa bb cc dd ee") == 1
+    assert Day4.solve("aa bb cc dd aaa") == 1
   end
 
   test "pass phrase with duplicate values is not valid" do
-    assert Day4.solve("aa bb cc dd aa") == false
+    assert Day4.solve("aa bb cc dd aa") == 0
+  end
+
+  test "2 out of 3 phrases are correct" do
+    assert Day4.solve(["aa bb cc dd aaa", "aa bb cc dd ee", "aa bb cc dd aa"]) == 2
   end
 end
